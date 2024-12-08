8d352d51668f8adb1eeb097ead45f97a105dedf7
Daniel Andrés Vélez Vargas <daniel_andres.velez@uao.edu.co>
2024-12-08 15:36:59 -0500

    passing props commonly and with propagation

---
+++ src/components/Text.svelte
@@ -0,0 +1,12 @@
+<script>
+  // Adding this prop as a fallback
+  export let textOne = "Lorem Ipsum";
+  export let textTwo = "Lorem Ipsum Two";
+</script>
+
+<div class="text">
+  <p>
+    {textOne}
+    {textTwo}
+  </p>
+</div>
