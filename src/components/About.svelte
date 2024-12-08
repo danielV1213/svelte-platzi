8efafac134cff74d6d19628858b008a66068283e
Daniel Andrés Vélez Vargas <daniel_andres.velez@uao.edu.co>
2024-12-08 15:05:15 -0500

    added reactivity example

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,11 +1,17 @@
 <script>
   let someText = `Frontend Developer at Globant`;
+  let count = 0;
+
+  function increase() {
+    count++;
+  }
 </script>
 
 <div class="about">
   <p>
     {someText}
   </p>
+  <button on:click={increase}>Click {count === 0 ? "" : count}</button>
 </div>
 
 <style>
