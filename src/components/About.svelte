5a2fcf6ffea9987b9a779aadb587e98d9cb5dd30
Daniel Andrés Vélez Vargas <daniel_andres.velez@uao.edu.co>
2024-12-08 16:03:53 -0500

    added conditional rendering

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,22 +1,38 @@
 <script>
   let someText = `Frontend Developer at Globant`;
   let count = 0;
+  let styles = { darkMode: false };
 
   function increase() {
     count++;
   }
+
+  function toggle() {
+    styles.darkMode = !styles.darkMode;
+    window.document.body.classList.toggle("dark-mode");
+  }
 </script>
 
 <div class="about">
-  <p>
-    {someText}
-  </p>
+  {#if !styles.darkMode}
+    <p class="light-mode-paragraph">{someText}</p>
+  {:else}
+    <p class="dark-mode-paragraph">Hello Dark Mode</p>
+  {/if}
+
   <button on:click={increase}>Click {count === 0 ? "" : count}</button>
+  <button on:click={toggle}>DarkMode</button>
 </div>
 
 <style>
   p {
-    color: var(--theme-color);
     font-size: 22px;
+  }
+  p.light-mode-paragraph {
+    color: var(--theme-color);
+  }
+
+  p.dark-mode-paragraph {
+    color: inherit;
   }
 </style>
